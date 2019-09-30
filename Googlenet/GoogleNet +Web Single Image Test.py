3a8f3d9d098e034dd4c348764f4105a98894f315
nywang2019 <55544699+nywang2019@users.noreply.github.com>
2019-09-30 15:53:29 -0400

    Create GoogleNet +Web Single Image Test.py

---
+++ Googlenet/GoogleNet +Web Single Image Test.py
@@ -0,0 +1,39 @@
+# NY 本程序用预训练好的GoogleNet 模型和参数实现网络单张图片的识别
+import torch
+import torchvision.models as models
+model=models.googlenet(pretrained=True, progress=True)
+model.eval()
+
+
+import urllib
+url, filename = ("https://github.com/pytorch/hub/raw/master/dog.jpg", "dog.jpg")
+try: urllib.URLopener().retrieve(url, filename)
+except: urllib.request.urlretrieve(url, filename)
+
+from PIL import Image
+from torchvision import transforms
+input_image = Image.open(filename)
+preprocess = transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
+input_tensor = preprocess(input_image)
+input_batch = input_tensor.unsqueeze(0) # create a mini-batch as expected by the model
+
+# move the input and model to GPU for speed if available
+if torch.cuda.is_available():
+    input_batch = input_batch.to('cuda')
+    model.to('cuda')
+
+with torch.no_grad():
+    output = model(input_batch)
+# Tensor of shape 1000, with confidence scores over Imagenet's 1000 classes
+print(output[0])
+# The output has unnormalized scores. To get probabilities, you can run a softmax on it.
+print(torch.nn.functional.softmax(output[0], dim=0))
+print(torch.max(output[0],0))
+
+# 保存训练好的模型参数和状态
+torch.save(model.state_dict(), 'model_para.pth')
